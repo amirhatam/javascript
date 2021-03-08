29be5c10a5453994a463c8b521def6979f0ee506
amirhatam <64803746+amirhatam@users.noreply.github.com>
2021-03-08 15:21:03 +0100

    Exercices

---
+++ jour-1/jour01.js
@@ -0,0 +1,55 @@
+// 01 - Hello World
+
+console.log("Hello World !");
+
+// 02 - String
+
+var test;
+test = "My name is Amir";
+console.log(test);
+
+// 03 - Concatenation
+
+var name;
+name = "Hatam"
+console.log("Nice to meet you " + name);
+
+// 04 - String length
+
+var testLength;
+testLength = "I'm very long !";
+console.log(testLength.length);
+
+
+// 05 - Replace
+
+var food;
+food = "croissant is meh";
+food = food.replace("meh", "so good");
+console.log(food);
+
+// 06 - Up and Down
+
+var basic = "this is Cool";
+var basicUp = "this is Cool";
+var basicDown = "this is Cool";
+
+
+console.log(basic + " " +  basicUp.toUpperCase() + " " + basicDown.toLowerCase());
+
+// 07 - Split 
+
+var word = "banana";
+var letters =word;
+
+console.log(letters.split(""));
+
+// 08 - Template
+
+var age = 34;
+var temlate = "I'm " + age + " years old";
+
+console.log(temlate)
+
+// Bonus
+
