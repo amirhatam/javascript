18bdfdbdfc1d2a6097fc24c9d3b20429cdec9cc4
amirhatam <64803746+amirhatam@users.noreply.github.com>
2021-03-10 17:24:28 +0100

    jour03

---
+++ jour-03/bonus-III.js
@@ -0,0 +1,4 @@
+var camarad = ["Abdou", "Amara", "Faruk", "Ezka", "Ikram", "N.A", "Francise", "Suleman", "Axel", "Lionel"];
+
+const random = Math.floor(Math.random() * camarad.length);
+console.log(random, camarad[random]);
