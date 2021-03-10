14568fc240598efdc1b4adc487a9bf7041b27842
amirhatam <64803746+amirhatam@users.noreply.github.com>
2021-03-10 17:04:34 +0100

    jour03

--- jour-03/ex-06.js
+++ jour-03/ex-06.js
@@ -1,13 +1,21 @@
 var sentence = "Hello Konexio !";
-resentence = ""
-// var i = sentence;
+var cele = [];
 
-// console.log(sentence.length);
-
-// for (var i = sentence.length -1; i = 0; i>=0; i--) {
-//     console.log(i);
-// }
-for (i = sentence.length -1; i >= 0; i--) {
-    resentence +- i
+for (var i=sentence.length-1; i>=0; i--) {
+    cele.push(sentence[i]);
 }
-console.log(resentence);
+console.log(cele.join(""));
+
+// var sentence = "Hello Konexio !";
+// resentence = ""
+// // var i = sentence;
+
+// // console.log(sentence.length);
+
+// // for (var i = sentence.length -1; i = 0; i>=0; i--) {
+// //     console.log(i);
+// // }
+// for (i = sentence.length -1; i >= 0; i--) {
+//     resentence +- i
+// }
+// console.log(resentence);
