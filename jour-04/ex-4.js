31ca79061de50373e2b06c3b7ec9d5dda682e3e4
amirhatam <64803746+amirhatam@users.noreply.github.com>
2021-03-11 16:19:21 +0100

    ex-5

--- jour-04/ex-4.js
+++ jour-04/ex-4.js
@@ -1,7 +1,13 @@
+var number1 = 10;
+var number2 = 20;
+
+
 function compare (num1, num2) {
-    if (num1 > num2) {
+    console.log(number1);
+    console.log(number2);
+    if (number1 > number2) {
         console.log("num1 is bigger");
-    } if (num1 < num2) {
+    } else if (number1 < number2) {
         console.log("num2 is bigger");
     } else {
        console.log("both are the same");
---
+++ jour-04/ex-5.js
@@ -0,0 +1,17 @@
+function addUp(num) {
+
+    var min = 0;
+    var max = 12;
+
+    for (var i = 0; i <= max; i++) {
+       
+        min += i;
+
+        console.log(min);
+    }
+
+    console.log(min);
+}
+
+
+addUp();
