23fd27de7eff53ca3dc2c671adeecddab88a58cc
amirhatam <64803746+amirhatam@users.noreply.github.com>
2021-03-17 10:54:32 +0100

    calculator

--- jour-5/Calculator/calculator.js
+++ jour-5/Calculator/calculator.js
@@ -1,24 +1,27 @@
-// console.log(process.argv);
-
-
-
-// for(i = 0 ; i<process)
-
-
-
-
 function calculate (test) {
 
   
-    // if (i > 0) {
-    //     console.log(parseInt(process.argv[2]) * parseInt(process.argv[3]));
-
-    // }
-
     var args = process.argv.slice(2);
 
+
+    if (process.argv[3]=== "+"){
+        console.log(parseInt(process.argv[2]) + parseInt(process.argv[4]));
+
+    }else if (process.argv[3]=== "-"){
+        console.log(parseInt(process.argv[2]) - parseInt(process.argv[4]));
+    }
+    else if (process.argv[3]=== "/"){
+        console.log(parseInt(process.argv[2]) / parseInt(process.argv[4]));
     
-    console.log(parseInt(process.argv[2]) / parseInt(process.argv[3]));
+    }
+    else if (process.argv[3]=== "*"){
+        console.log(parseInt(process.argv[2]) * parseInt(process.argv[4]));
+
+    }
+    else {
+        console.log("error");
+    }
+    
 
 }
 
