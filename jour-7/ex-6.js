ac8c564caab00c06a9a0b43b2e05c525cf5aca87
Amir <amirhatam136@gmail.com>
2021-03-22 17:25:04 +0100

    ex-6

---
+++ jour-7/ex-6.js
@@ -0,0 +1,42 @@
+var cakes = [
+	{
+		name: "cake",
+		flavor: "vanilla",
+		status: "available"
+	},
+	{
+		name: "brownie",
+		flavor: "chocolate",
+		status: "available"
+	},
+	{
+		name: "pie",
+		flavor: "strawberry",
+		status: "available"
+	},
+	{
+		name: "muffin",
+		flavor: "pistachio",
+		status: "available"
+	},
+	{
+		name: "donut",
+		flavor: "chocolate",
+		status: "available"
+	},
+];
+
+
+var soldOut = cakes.filter(function(sold){
+
+    return sold.flavor == "chocolate"
+    
+
+});
+
+soldOut.map(function(y) {
+
+    return y.status = "sold out"
+})
+
+console.log(soldOut);
